78f2ccf593d86c699b2406d36cddbc3d078cd59d
Bhavan Turaka <bhavanturaka21@gmail.com>
2017-11-01 03:43:30 +0530

    Created Models

--- class/admin.py
+++ class/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
-# Register your models here.
+# UserName : admin
+# Password : fordis1234
+
+from .models import Class
+from .models import ClassInsRelation
+from .models import Joins
+from .models import Topic
+from .models import Post
+from .models import ViewRelation
+from .models import PinRelation
+from .models import TopicPostRelation
+from .models import Comment
+from .models import Poll
+from .models import Option
+from .models import OptionStudRelation
+
+admin.site.register(Class)
+admin.site.register(ClassInsRelation)
+admin.site.register(Joins)
+admin.site.register(Topic)
+admin.site.register(Post)
+admin.site.register(ViewRelation)
+admin.site.register(PinRelation)
+admin.site.register(TopicPostRelation)
+admin.site.register(Comment)
+admin.site.register(Poll)
+admin.site.register(Option)
+admin.site.register(OptionStudRelation)
--- class/models.py
+++ class/models.py
@@ -1,3 +1,78 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Class(models.Model):
+    course_name = models.CharField(max_length=100)
+    course_code = models.CharField(max_length=10)
+    year = models.CharField(max_length=4)
+    semester = models.CharField(max_length=10)
+    class_code = models.CharField(max_length=6, unique=True)
+    class_password = models.CharField(max_length=50)
+    is_active = models.BooleanField(default=1)
+
+
+class ClassInsRelation(models.Model):
+	class_id = models.ForeignKey(Class, on_delete=models.CASCADE)
+	ins_id = models.ForeignKey(User, on_delete=models.CASCADE)
+	time_stamp = models.DateTimeField()
+
+#students
+class Joins(models.Model):
+	class_id = models.ForeignKey(Class, on_delete=models.CASCADE)
+	stud_id = models.ForeignKey(User, on_delete=models.CASCADE)
+	time_stamp = models.DateTimeField()
+
+class Topic(models.Model):
+	name = models.CharField(max_length=50)
+	class_id = models.ForeignKey(Class, on_delete=models.CASCADE)
+
+	class Meta:
+		unique_together = ('name', 'class_id',)
+
+
+class Post(models.Model):
+	class_id = models.ForeignKey(Class, on_delete=models.CASCADE)
+	posted_by = models.ForeignKey(User, on_delete=models.CASCADE)
+	title = models.CharField(max_length=50)
+	content = models.TextField()
+	is_draft = models.BooleanField(default=False)
+	views = models.IntegerField(default=0)
+	sched_time = models.DateTimeField(null=True)
+	time_stamp = models.DateTimeField()
+
+class ViewRelation(models.Model):
+	user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+	post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+class PinRelation(models.Model):
+	user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+	post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+class TopicPostRelation(models.Model):
+	topic_id = models.ForeignKey(Topic, on_delete=models.CASCADE)
+	post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+class Comment(models.Model):
+	post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
+	posted_by = models.ForeignKey(User, on_delete=models.CASCADE)
+	content = models.TextField()
+	time_stamp = models.DateTimeField()
+
+class Poll(models.Model):
+	class_id = models.ForeignKey(Class, on_delete=models.CASCADE)
+	posted_by = models.ForeignKey(User, on_delete=models.CASCADE)
+	title = models.CharField(max_length=50)
+	check_multiple = models.BooleanField()
+	attempts = models.IntegerField(default=0)
+	deadline = models.DateTimeField()
+	time_stamp = models.DateTimeField()
+
+class Option(models.Model):
+	poll_id = models.ForeignKey(Post, on_delete=models.CASCADE)
+	content = models.CharField(max_length=200)
+	count = models.IntegerField(default=0)
+
+class OptionStudRelation(models.Model):
+	option_id =  models.ForeignKey(Option, on_delete=models.CASCADE)
+	stud_id = models.ForeignKey(User, on_delete=models.CASCADE)
